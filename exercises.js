0c04eb6e4be7c2dfad8c78cac95b4384eccce9c2
Luke Fiorio <lukefiorio@Lukes-MacBook-Pro.local>
2019-03-10 21:23:30 -1000

    tupleConvToObj

--- exercises.js
+++ exercises.js
@@ -716,6 +716,15 @@ console.log(frequencyMap(testStrArr));
  */
 var tupleConvertToObject;
 
+tupleConvertToObject = function(arr) {
+  var obj={
+  }
+  for (var i=0;i<arr.length;i++) {
+    obj[arr[i][0]] = arr[i][1];
+  }
+  return obj;
+}
+console.log(tupleConvertToObject([['a',1],['b',2],['c',3]]));
 
 module.exports = {
   doubleArray: doubleArray,
